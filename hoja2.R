64b0e47c08370c2e36e1bd2bddf32efe28672161
Ivette <car16020@uvg.edu.gt>
2019-02-25 14:40:22 -0600

    hoja en r

---
+++ hoja2.R
@@ -0,0 +1,7 @@
+#Para obtener las graficas de codo 
+#movies[,c(3:5,)]
+#var <-  (columnas -1 ) * suma(varianza de todas las columnas)
+#for (i in 2:10): 
+# var[i] = sum(kmeans($withinss))
+
+#plot(1:10, var, "b")
